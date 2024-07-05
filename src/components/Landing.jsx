388ed0b051652bd70efe32ebedb01bb43c7825bb
Kolapo Wariz <kolapowariz@gmail.com>
2024-07-05 04:37:50 +0100

    new

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,6 +4,10 @@ import { MdCancel } from "react-icons/md";
 import Myself2 from '/Myself2.jpeg';
 import nft from '/nft.jpg';
 import { Link } from 'react-router-dom';
+import fb from '/fb.svg';
+import ig from '/insta.svg';
+import tw from '/Group.svg';
+import ln from '/Linkedin.svg';
 
 const Landing = () => {
   const [click, setClick] = useState(false)
@@ -13,11 +17,11 @@ const Landing = () => {
   }
 
   return (
-    <div className='w-full mx-auto lg:w-[95%] lg:mx-auto bg-[#ffbb8b] p-5 m-5'>
-      <header className='lg:hidden flex justify-between mt-4 mx-4'>
+    <div className='w-full mx-auto lg:w-[95%] lg:mx-auto bg-[#ffbb8b] p-5 lg:m-5'>
+      <header className='lg:hidden flex justify-between items-center mt-4 mx-4'>
         <h1 className='text-sm'>Kolapo Wariz</h1>
         <nav className='flex flex-col justify-center items-end'>
-          <button onClick={toggle}>{click ? <MdCancel className='w-8 h-8 border' /> : <FiAlignJustify className='w-8 h-8 border' />}</button>
+          <button onClick={toggle}>{click ? <MdCancel className='w-8 h-8 ' /> : <FiAlignJustify className='w-8 h-8' />}</button>
         </nav>
       </header>
       <main className='lg:hidden'>
@@ -47,7 +51,7 @@ const Landing = () => {
         <section id='#' className='flex flex-col lg:flex-row lg:justify-between lg:items-start'>
           <section className='order-last lg:order-first lg:w-3/5'>
             <p className='text-2xl lg:text-3xl my-4'>Hi there! Call me Wariz,</p>
-            <p className='text-4xl lg:text-4xl my-4'>FrontEnd developer.</p>
+            <p className='text-3xl lg:text-4xl my-4'>FrontEnd developer.</p>
             <section id='about' className='mt-5'>
               <p className='mt-2'>I&apos;m a passionate and dedicated Frontend Developer who loves creating beautiful, dynamic user and functional web experiences. I enjoy transforming ideas into reality and thrive on solving complex problems with elegant solutions.</p>
               <p>During my teenage years, at the age of 17, I became fascinated with the world of programming and I delved into it intending to learn C#. Although I encountered obstacles and my progress was not as smooth as I had envisioned, this experience laid the foundation for my future endeavors. Despite encountering numerous challenges, I persisted in my pursuit. However, my journey took an unexpected turn when I stumbled upon JavaScript, a language that ignited a deep-seated passion within me. From that point on, My path was crystal clear - I was meant to pursue a career in web development. This newfound passion has been the driving force behind my relentless quest for self-improvement, pushing me to continually refine my skills, keep abreast of the latest industry trends, and adhere to best practices in the field.</p>
@@ -61,32 +65,45 @@ const Landing = () => {
         <section id='projects' className='mt-4'>
           <h2 className='text-center text-4xl'>Projects</h2>
           <Link to='/projects' className='flex flex-col lg:flex-row justify-center items-center my-10 '>
-            <div className='w-4/5 h-72 rounded-t-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-s-2xl p-5'>
+            <div className='w-full h-72 rounded-t-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-s-2xl p-5'>
               <p>Healler</p>
               <p>Description of Healler</p>
+              <p>View Project</p>
             </div>
-            <div className='w-4/5 h-72 rounded-b-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-e-2xl p-5'>
+            <div className='w-full h-72 rounded-b-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-e-2xl p-5'>
               <img src={nft} alt="myself" className='h-full mx-auto' />
             </div>
           </Link>
           <Link to='/projects' className='flex flex-col lg:flex-row justify-center items-center my-10'>
-            <div className='w-4/5 h-72 rounded-b-xl lg:w-96 lg:h-96 bg-gray-200 lg:rounded-r-2xl lg:rounded-l-none p-5'>
+            <div className='w-full h-72 rounded-b-xl lg:w-96 lg:h-96 bg-gray-200 lg:rounded-r-2xl lg:rounded-l-none p-5'>
               <p>Capstone Project</p>
               <p>Description of Capstone project</p>
+              <p>View Project</p>
             </div>
-            <div className='w-4/5 h-72 rounded-t-xl lg:w-96 lg:h-96 bg-gray-200 lg:rounded-l-2xl lg:rounded-r-none p-5 order-first'>
+            <div className='w-full h-72 rounded-t-xl lg:w-96 lg:h-96 bg-gray-200 lg:rounded-l-2xl lg:rounded-r-none p-5 order-first'>
               <img src={nft} alt="myself" className='h-full mx-auto' />
             </div>
           </Link>
-          <p>Project section is under maintanace</p>
         </section>
-        <section id='contact' className='mt-4'>
+        <section id='contact' className='mt-4 w-4/5 lg:w-2/4 mx-auto'>
           <h2 className='text-center text-4xl'>Contact</h2>
-          <p>Contact section is under maintanace</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia delectus cum deserunt dolores repellat sequi corrupti dolorum a facere numquam, id in, illum ab ullam debitis assumenda reprehenderit molestias cumque.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, quas quam sunt unde neque eveniet commodi recusandae, delectus suscipit quod consequatur culpa quo necessitatibus ullam a, maxime in aspernatur. Placeat?</p>
+          <form action="">
+            <input type="text" placeholder='Name' className='w-full p-2 my-2 rounded-md' />
+            <input type="email" placeholder='Email' className='w-full p-2 my-2 rounded-md' />
+            <textarea name="" id="" cols="30" rows="10" placeholder='Message' className='w-full p-2 my-2 rounded-md'></textarea>
+            <button type="button" className='w-full p-2 my-2 bg-blue-500 text-white rounded-md'>Send</button>
+          </form>
         </section>
       </main>
+      <footer>
+        <div className='flex justify-center'>
+          <img src={fb} alt="Facebook Account" />
+          <img src={ig} alt="Instagram Account" />
+          <img src={tw} alt="X Account" />
+          <img src={ln} alt="Facebook Account" />
+        </div>
+        <p className='text-center text-sm my-4'>Kolapo Wariz &copy; 2024</p>
+      </footer>
     </div>
 
   )
