d570898dabae91e7419e56bb368a423db53d2176
Kolapo Wariz <kolapowariz@gmail.com>
2024-07-06 01:36:00 +0100

    new

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,6 +8,7 @@ import fb from '/fb.svg';
 import ig from '/insta.svg';
 import tw from '/Group.svg';
 import ln from '/Linkedin.svg';
+import portdata from '../Data/portfolio.json';
 
 const Landing = () => {
   const [click, setClick] = useState(false)
@@ -54,36 +55,57 @@ const Landing = () => {
             <p className='text-3xl lg:text-4xl my-4'>FrontEnd developer.</p>
             <section id='about' className='mt-5'>
               <p className='mt-2'>I&apos;m a passionate and dedicated Frontend Developer who loves creating beautiful, dynamic user and functional web experiences. I enjoy transforming ideas into reality and thrive on solving complex problems with elegant solutions.</p>
-              <p>During my teenage years, at the age of 17, I became fascinated with the world of programming and I delved into it intending to learn C#. Although I encountered obstacles and my progress was not as smooth as I had envisioned, this experience laid the foundation for my future endeavors. Despite encountering numerous challenges, I persisted in my pursuit. However, my journey took an unexpected turn when I stumbled upon JavaScript, a language that ignited a deep-seated passion within me. From that point on, My path was crystal clear - I was meant to pursue a career in web development. This newfound passion has been the driving force behind my relentless quest for self-improvement, pushing me to continually refine my skills, keep abreast of the latest industry trends, and adhere to best practices in the field.</p>
+              <p>During my teenage years, at the age of 17, I became fascinated with the world of programming and I delved into it intending to learn C# (C Sharp) programming language. Although I encountered obstacles and my progress was not as smooth as I had envisioned, this experience laid the foundation for my future endeavors. Despite encountering numerous challenges, I persisted in my pursuit.</p>
+              <p>However, my journey took an unexpected turn when I stumbled upon JavaScript, a language that ignited a deep-seated passion within me. From that point on, My path was crystal clear - I was meant to pursue a career in web development.</p>
+              <p>This newfound passion has been the driving force behind my relentless quest for self-improvement, pushing me to continually refine my skills, keep abreast of the latest industry trends, and adhere to best practices in the field.</p>
             </section>
           </section>
           <section>
-            <img src={Myself2} alt="Kolapo Wariz" loading='lazy' className='block mt-6 h-72 w-72 mx-auto rounded-full object-cover' />
-            {/* <img src={Myself2} alt="Kolapo Wariz" className='mt-6 h-72 w-72 mx-auto rounded-full object-cover' /> */}
+            <img src={Myself2} width={779} height={571} alt="Kolapo Wariz" loading='lazy' className='block mt-6 h-72 w-72 mx-auto rounded-full object-cover' />
           </section>
         </section>
         <section id='projects' className='mt-4'>
           <h2 className='text-center text-4xl'>Projects</h2>
-          <Link to='/projects' className='flex flex-col lg:flex-row justify-center items-center my-10 '>
+          <section className='flex flex-col lg:flex-row justify-center items-center my-10 '>
             <div className='w-full h-72 rounded-t-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-s-2xl p-5'>
               <p>Healler</p>
-              <p>Description of Healler</p>
-              <p>View Project</p>
+              <p className='mb-4'>Description of Healler</p>
+              <Link to='/projects' className='text-blue-500 border-slate-800 border rounded-lg p-2'>View Project</Link>
             </div>
             <div className='w-full h-72 rounded-b-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-e-2xl p-5'>
               <img src={nft} alt="myself" className='h-full mx-auto' />
             </div>
-          </Link>
-          <Link to='/projects' className='flex flex-col lg:flex-row justify-center items-center my-10'>
+          </section>
+          <section className='flex flex-col lg:flex-row justify-center items-center my-10'>
             <div className='w-full h-72 rounded-b-xl lg:w-96 lg:h-96 bg-gray-200 lg:rounded-r-2xl lg:rounded-l-none p-5'>
               <p>Capstone Project</p>
-              <p>Description of Capstone project</p>
-              <p>View Project</p>
+              <p className='mb-4'>Description of Capstone project</p>
+              <Link to='/projects' className='text-blue-500 border-slate-800 border rounded-lg p-2'>View Project</Link>
             </div>
             <div className='w-full h-72 rounded-t-xl lg:w-96 lg:h-96 bg-gray-200 lg:rounded-l-2xl lg:rounded-r-none p-5 order-first'>
               <img src={nft} alt="myself" className='h-full mx-auto' />
             </div>
-          </Link>
+          </section>
+        </section>
+        <section>
+          {/* <h2 className='text-center text-4xl'>Projects</h2> */}
+          <div>
+            {portdata.portfolio.map((data, index) => {
+              return (
+                <div key={index} className='flex flex-col lg:flex-row justify-center items-center my-10 '>
+                  <div className='w-full h-72 rounded-t-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-s-2xl p-5'>
+                    <p>{data.title}</p>
+                    <p className='mb-4'>{data.description}</p>
+                    <Link to='/projects' className='text-blue-500 border-slate-800 border rounded-lg p-2'>View Project</Link>
+                  </div>
+                  <div className='w-full h-72 rounded-b-xl lg:rounded-none lg:w-96 lg:h-96 bg-gray-200 lg:rounded-e-2xl p-5'>
+                    <img src={data.image} alt={data['image-alt']} width={338} height={344} className='h-full mx-auto' />
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
         </section>
         <section id='contact' className='mt-4 w-4/5 lg:w-2/4 mx-auto'>
           <h2 className='text-center text-4xl'>Contact</h2>
@@ -97,10 +119,10 @@ const Landing = () => {
       </main>
       <footer>
         <div className='flex justify-center'>
-          <img src={fb} alt="Facebook Account" />
-          <img src={ig} alt="Instagram Account" />
-          <img src={tw} alt="X Account" />
-          <img src={ln} alt="Facebook Account" />
+          <img src={fb} width={30} height={30} className='mr-4' alt="Facebook Account" />
+          <img src={ig} width={30} height={30} className='mr-4' alt="Instagram Account" />
+          <img src={tw} width={37} height={30} className='mr-4' alt="X Account" />
+          <img src={ln} width={30} height={30} className='mr-4' alt="Facebook Account" />
         </div>
         <p className='text-center text-sm my-4'>Kolapo Wariz &copy; 2024</p>
       </footer>
